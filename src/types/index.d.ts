c64ee91f71d9062db13ecbddbf7a2fc1b7a1fcb5
taewoo01 <133633356+taewoo01@users.noreply.github.com>
2025-08-27 13:46:53 +0900

    Update index.d.ts

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,9 +1,12 @@
+import * as express from "express";
+
 declare global {
   namespace Express {
     interface Request {
       user?: {
         id: number;
         email: string;
+        nickname?: string; // 필요 시 추가
       };
     }
   }
