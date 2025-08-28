60b8fd947878f2b036652898159bdfd83a57a604
taewoo01 <133633356+taewoo01@users.noreply.github.com>
2025-08-28 21:53:21 +0900

    Update pool.ts

--- src/db/pool.ts
+++ src/db/pool.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 export const pool = new Pool({
   host: process.env.DB_HOST,       
-  port: Number(process.env.DB_PORT) 
+  port: Number(process.env.DB_PORT), 
   user: process.env.DB_USER,       
   password: process.env.DB_PASS,   
   database: process.env.DB_NAME,   
