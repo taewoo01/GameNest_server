10cee6ace36f7e12f54e4d298ad2d1381e685f78
taewoo01 <133633356+taewoo01@users.noreply.github.com>
2025-09-01 13:42:07 +0900

    Update pool.ts

--- src/db/pool.ts
+++ src/db/pool.ts
@@ -1,15 +1,17 @@
-import { Pool } from "pg";
+// src/db.ts
+import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-export const pool = new Pool({
-  host: process.env.DB_HOST,       
-  port: Number(process.env.DB_PORT), 
-  user: process.env.DB_USER,       
-  password: process.env.DB_PASS,   
-  database: process.env.DB_NAME,   
-  max: 10,                         
+export const pool = mysql.createPool({
+  host: process.env.DB_HOST as string,
+  port: Number(process.env.DB_PORT) || 3306,
+  user: process.env.DB_USER as string,
+  password: process.env.DB_PASS as string,
+  database: process.env.DB_NAME as string,
+  waitForConnections: true,
+  connectionLimit: 10,
 });
 
 export default pool;
