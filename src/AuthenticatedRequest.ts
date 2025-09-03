e88451be22d0e8aec69afe5632619d1e436ebe86
taewoo01 <133633356+taewoo01@users.noreply.github.com>
2025-09-03 14:27:28 +0900

    Update AuthenticatedRequest.ts

--- src/AuthenticatedRequest.ts
+++ src/AuthenticatedRequest.ts
@@ -1,6 +1,11 @@
 import { Request } from "express";
 
-export interface AuthenticatedRequest extends Request {
+export interface AuthenticatedRequest<
+  P = any,
+  ResBody = any,
+  ReqBody = any,
+  ReqQuery = any
+> extends Request<P, ResBody, ReqBody, ReqQuery> {
   user: {
     id: number;
     email: string;
